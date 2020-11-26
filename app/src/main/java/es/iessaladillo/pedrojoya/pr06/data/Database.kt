5b9aae0b5ca74184a0fbff88c2c52edde06511d8
Ismael Quirós <ismaeljesusquiros@gmail.com>
2020-11-26 09:30:32 +0100

    v0.1
    Hecha la base de datos

--- app/src/main/java/es/iessaladillo/pedrojoya/pr06/data/Database.kt
+++ app/src/main/java/es/iessaladillo/pedrojoya/pr06/data/Database.kt
@@ -1,5 +1,53 @@
 package es.iessaladillo.pedrojoya.pr06.data
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import es.iessaladillo.pedrojoya.pr06.data.model.User
+
+class Database : DataSource{
+    private val list : MutableList<User> = mutableListOf()
+    private val listData : MutableLiveData<List<User>> = MutableLiveData<List<User>>()
+    private var id : Long = 0
+
+    init {
+        orderList()
+    }
+
+    override fun getAllUsersOrderedByName(): LiveData<List<User>> {
+        return listData
+    }
+
+    private fun orderList(){
+        listData.value = list.sortedBy { it.name }
+    }
+
+    private fun actData(){
+        listData.value = list
+    }
+
+    override fun insertUser(user: User) {
+        user.id = ++id
+        list.add(user)
+        actData()
+    }
+
+    override fun updateUser(user: User) {
+        val indexUser = list.indexOfFirst {it.id == user.id}
+        if(indexUser != -1){
+            list[indexUser] = user
+            actData()
+        }
+    }
+
+    override fun deleteUser(user: User) {
+        val indexUser = list.indexOfFirst {it.id == user.id}
+        if(indexUser != -1){
+            list.removeAt(indexUser)
+            actData()
+        }
+    }
+
+}
 // TODO:
 //  Crear una singleton Database que implemente la interfaz DataSource.
 //  Al insertar un usuario, se le asignará un id autonumérico
--- app/src/main/java/es/iessaladillo/pedrojoya/pr06/data/model/User.kt
+++ app/src/main/java/es/iessaladillo/pedrojoya/pr06/data/model/User.kt
@@ -1,6 +1,20 @@
 package es.iessaladillo.pedrojoya.pr06.data.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
 // TODO:
 //  Crear una clase de datos User que implemente Parcelable y que
 //  contenga el id de tipo Long, nombre, email, phoneNumber, address, web y photoUrl
 //  todas ellas de tipo String.
+
+@Parcelize
+data class User(
+        var id : Long,
+        var name : String,
+        var email : String,
+        var phoneNumber : String,
+        var adress : String,
+        var web : String,
+        var photoUrl : String,
+) : Parcelable
